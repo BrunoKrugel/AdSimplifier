f3604a07f942f71309cb0ec54487e9d3edcf3af2
Bruno Krugel <bruno.krugel@ifood.com.br>
2023-01-25 23:52:26 -0300

    env

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -8,8 +8,8 @@ console.log(process.env.FACEBOOK_ID);
 export default NextAuth({
   providers: [
     FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET
+      clientId: process.env.NEXT_PUBLIC_FACEBOOK_ID,
+      clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_SECRET
     }),
   ]
 })
