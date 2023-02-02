4d143d7914aeb983d55360737016c3cd131cc4e3
Bruno Krugel <brunokrugel@gmail.com>
2023-02-02 15:01:23 -0300

    api authentication

--- src/pages/api/mongo/login.js
+++ src/pages/api/mongo/login.js
@@ -1,39 +1,30 @@
 import clientPromise from '@/lib/mongo/mongo';
-import { status } from '@/constants/constants';
 
-async function authenticate(req, res) {
-  console.log('authenticate')
-  return new Promise((resolve, reject) => {
-    clientPromise
-      .then((client) => {
-        client
-          .db('account')
-          .collection('user')
-          .findOne(
-            {
-              username: req.body.username,
-              password: req.body.password,
-            },
-            function (err, result) {
-              if (err || !result) {
-                console.log(err)
-                status.code = 403;
-                status.message = 'Usuário não existe';
-                res.send(status);
-                resolve();
-              } else {
-                status.code = 200;
-                res.send(status);
-                reject();
-              }
-            }
-          );
-      })
-      .catch((err) => {
-        console.log(err);
-        reject(err);
-      });
+export default async function authenticate(req, res) {
+  const client = await clientPromise;
+  const isAuthPromise = new Promise((resolve, reject) => {
+    client
+      .db('account')
+      .collection('user')
+      .findOne(
+        {
+          username: req.body.username,
+          password: req.body.password,
+        },
+        function (err, result) {
+          if (err || !result) {
+            reject();
+          } else {
+            resolve();
+          }
+        }
+      );
   });
-}
 
-export { authenticate };
+  try {
+    await isAuthPromise;
+    res.status(200).send();
+  } catch (error) {
+    res.status(401).send();
+  }
+}
