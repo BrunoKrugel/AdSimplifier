933e415781b683338710d00caa15c28594a260db
Bruno Krugel <brunokrugel@gmail.com>
2023-02-05 18:13:34 -0300

    Update color.js

--- src/lib/chart/color.js
+++ src/lib/chart/color.js
@@ -1,14 +1,13 @@
 function randomChartColor() {
-  let color = Math.round,
-    s = 200;
-  let r = color(Math.random() * s);
-  let g = color(Math.random() * s);
-  let b = color(Math.random() * s);
+  const color = Math.round
+  const s = 200
+
+  const [r, g, b] = [color(Math.random() * s), color(Math.random() * s), color(Math.random() * s)]
+
   return {
-    rgb: 'rgb(' + r + ',' + g + ',' + b + ')',
-    rgba:
-      'rgba(' + r + ',' + g + ',' + b + ', 0.5)',
-  };
+    rgb: `rgb(${r},${g},${b})`,
+    rgba: `rgba(${r},${g},${b}, 0.5)`,
+  }
 }
 
-export { randomChartColor };
+export { randomChartColor }
