61f7887aa35d9b3e197d39ba362fe946e862a044
Bruno Krugel <brunokrugel@gmail.com>
2023-02-05 18:13:03 -0300

    improve chart component

--- src/components/chart/chart.js
+++ src/components/chart/chart.js
@@ -1,94 +1,26 @@
-import React from 'react';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-} from 'chart.js';
+import React from 'react'
+import { Bar, Line } from 'react-chartjs-2'
 
-import { Line, Bar } from 'react-chartjs-2';
-import { faker } from '@faker-js/faker';
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
-
-const Chart = (props) => {
+const Chart = props => {
   const options = {
     responsive: true,
-    plugins: {
-      legend: {
-        position: 'bottom',
-      },
-      title: {
-        display: true,
-        text: 'Ofertas',
-      },
+    legend: {
+      position: 'bottom',
     },
-  };
-  let labels = [];
-  let dataset = [];
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-  ];
-  if (props.labels != null) {
-    labels = props.labels;
-  } else {
-    labels = months;
+    title: {
+      display: true,
+      text: 'Ofertas',
+    },
   }
 
-  if (props.dataset == null) {
-    dataset = [
-      {
-        label: 'Campanha 1',
-        data: labels.map(() =>
-          faker.datatype.number({ min: -1000, max: 1000 })
-        ),
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-      {
-        label: 'Campanha 2',
-        data: labels.map(() =>
-          faker.datatype.number({ min: -1000, max: 1000 })
-        ),
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      },
-    ];
-  }
-    else { 
-      dataset = props.dataset;
-  }
+  const { labels = [], dataset = [] } = props
 
   const data = {
     labels,
     datasets: dataset,
-  };
+  }
 
-  return (
-    //type 10 = line, 20 = bar
-    props.type == 20 ? <Bar data={data} options={options} /> :
-    <Line data={data} options={options} />
-  );
-};
+  return props.type === 20 ? <Bar data={data} options={options} /> : <Line data={data} options={options} />
+}
 
-export default Chart;
+export default Chart
