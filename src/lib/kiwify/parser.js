760a7cca3c7410e65bd3f165b9577ee7d1e5edb4
Bruno Krugel <brunokrugel@gmail.com>
2023-02-05 14:26:36 -0300

    Skynet

--- src/lib/kiwify/parser.js
+++ src/lib/kiwify/parser.js
@@ -3,47 +3,40 @@ import { randomChartColor } from '@/lib/chart/color';
 function parseSalesToChart(arr, arrDates) {
   const salesMap = new Map();
 
-  arrDates.forEach((date) => {
-    arr.forEach((item) => {
+  arrDates.forEach(date => {
+    arr.forEach(item => {
       if (date === item.date.split('T')[0]) {
-        let sales = salesMap.get(item.product_name);
-        if (!sales) {
-          sales = [];
-        }
+        let sales = salesMap.get(item.product_name) || [];
         sales.push({
           name: item.product_name,
-          date: date,
+          date,
           sales: item.sales_number,
         });
         salesMap.set(item.product_name, sales);
       }
     });
   });
-  let chartData = organizeDateToChart(salesMap, arrDates);
-  return { chartData, arrDates };
+  return { 
+    chartData: organizeDateToChart(salesMap, arrDates),
+    arrDates 
+  };
 }
 
 function organizeDateToChart(salesMap, arrDates) {
-  let datasets = [];
-  let salesMapKeys = Array.from(salesMap.keys());
-  salesMapKeys.forEach((key) => {
-    let color = randomChartColor();
-    let saleData = new Array(arrDates.length).fill(0);
-    for (let i = 0; i < arrDates.length; i++) {
-      salesMap.get(key).forEach((item) => {
-        if (item.date === arrDates[i]) {
-          saleData[i] = item.sales;
-        }
-      });
-    }
-    datasets.push({
+  return Array.from(salesMap.keys()).map(key => {
+    const color = randomChartColor();
+    const sales = salesMap.get(key);
+    const saleData = arrDates.map(date => 
+      sales.find(item => item.date === date)?.sales || 0
+    );
+
+    return {
       label: key,
       data: saleData,
       borderColor: color.rgb,
       backgroundColor: color.rgba,
-    });
+    };
   });
-  return datasets;
 }
 
 export { parseSalesToChart };
