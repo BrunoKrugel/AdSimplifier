d050461eb2d80c5f6837795435605c4016a2d8aa
Bruno Krugel <brunokrugel@gmail.com>
2023-02-06 10:23:35 -0300

    skynet

--- src/lib/dates/date.js
+++ src/lib/dates/date.js
@@ -1,14 +1,16 @@
-import dayjs from 'dayjs';
+import dayjs from 'dayjs'
 
 function ArrayOfDates(startDate, endDate) {
-  let dateArray = [];
-  let currentDate = dayjs(startDate);
-  let stopDate = dayjs(endDate);
+  const dateArray = []
+  const stopDate = dayjs(endDate)
+  let currentDate = dayjs(startDate)
+
   while (currentDate <= stopDate) {
-    dateArray.push(dayjs(currentDate).format('YYYY-MM-DD'));
-    currentDate = dayjs(currentDate).add(1, 'days');
+    dateArray.push(currentDate.format('YYYY-MM-DD'))
+    currentDate = currentDate.add(1, 'days')
   }
-  return dateArray;
+
+  return dateArray
 }
 
-export { ArrayOfDates };
+export { ArrayOfDates }
