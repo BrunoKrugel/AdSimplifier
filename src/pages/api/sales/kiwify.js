2b3dadf5df5eb29d5f2aa476bdf149dc06610d9e
Bruno Krugel <brunokrugel@gmail.com>
2023-02-23 20:44:27 -0300

    Fix query from mongo

--- src/pages/api/sales/kiwify.js
+++ src/pages/api/sales/kiwify.js
@@ -4,33 +4,22 @@ import { encodeBase64 } from '@/lib/string/base64';
 // {$gte:ISODate(“2020-03-01”),$lt:ISODate(“2021-03-31”)}}
 //date_created: {"$gte": new Date("2015-10-01T00:00:00.000Z") , "$lt": new Date("2017-03-13T16:17:36.470Z") }});
 export default async function getSales(req, res) {
-  return new Promise((resolve, reject) => {
-    clientPromise
-      .then((client) => {
-        client
-          .db('kiwify-go')
-          .collection('sales')
-          .find({
-            user_id: encodeBase64(req.body.user_id),
-            date: {
-              $gte: new Date(req.body.iniDate + 'T00:00:00.000Z'),
-              $lt: new Date(req.body.endDate + 'T00:00:01.000Z'),
-            },
-          })
-          .toArray(function (err, result) {
-            if (err || !result) {
-              res.status(404).send();
-              reject();
-            } else {
-              res.status(200).send(result);
-              resolve();
-            }
-          });
+  const client = await clientPromise;
+  try {
+    const data = await client
+      .db('kiwify-go')
+      .collection('sales')
+      .find({
+        user_id: encodeBase64(req.body.user_id),
+        date: {
+          $gte: new Date(req.body.iniDate + 'T00:00:00.000Z'),
+          $lt: new Date(req.body.endDate + 'T00:00:01.000Z'),
+        },
       })
-      .catch((err) => {
-        reject(err);
-      });
-  });
+      .toArray();
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error retrieving data from database' });
+  }
 }
-
-export { getSales };
