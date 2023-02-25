cc767da681169b6c253ac1335db164b84ef7b98c
Bruno Krugel <brunokrugel@gmail.com>
2023-02-25 19:03:55 -0300

    Remove props types

--- src/components/micros/IntegrationBox/IntegrationBox.js
+++ src/components/micros/IntegrationBox/IntegrationBox.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Box, Button, CardMedia } from '@mui/material';
-import { padding } from '@mui/system';
 
-const IntegrationBox = ({ buttonText, onClick }) => {
+const IntegrationBox = (props) => {
   return (
     <Box
       sx={{
@@ -25,16 +23,11 @@ const IntegrationBox = ({ buttonText, onClick }) => {
       }}
     >
       <CardMedia component="img" image="/Kiwify.svg" sx={{ width: 45 }} />
-      {buttonText}
-      <Button variant="contained" onClick={onClick}>
+      {props.buttonText}
+      <Button variant="contained" onClick={props.onClick}>
         Instalar
       </Button>
     </Box>
   );
-};
-
-IntegrationBox.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  buttonText: PropTypes.string.isRequired,
 };
 export { IntegrationBox };
