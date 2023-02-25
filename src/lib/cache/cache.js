77cf5d50fddafed4c2ba17861a877a5fd3da39be
Bruno Krugel <brunokrugel@gmail.com>
2023-02-25 02:35:56 -0300

    Move functions to cache

---
+++ src/lib/cache/cache.js
@@ -0,0 +1,47 @@
+import keyv from '@/lib/cache/keyv';
+
+async function storeDate(initial, end) {
+    try {
+      const clientCache = await keyv;
+      await clientCache.set('initialDate', initial);
+      await clientCache.set('endDate', end);
+    } catch (error) {
+      console.error(`Error storing dates: ${error}`);
+    }
+  }
+  
+  async function storeArray(array) {
+    try {
+      const clientCache = await keyv;
+      await clientCache.set('sales', array);
+    } catch (error) {
+      console.error(`Error storing array: ${error}`);
+    }
+  }
+  
+  async function isDateCached(initial, end) {
+    try {
+      const clientCache = await keyv;
+      const [initialDate, endDate] = await Promise.all([
+        clientCache.get('initialDate'),
+        clientCache.get('endDate'),
+      ]);
+      return initialDate === initial && endDate === end;
+    } catch (error) {
+      console.error(`Error checking date cache: ${error}`);
+      return false;
+    }
+  }
+  
+  async function isCached() {
+    try {
+      const clientCache = await keyv;
+      const sales = await clientCache.get('sales');
+      return Boolean(sales);
+    } catch (error) {
+      console.error(`Error checking sales cache: ${error}`);
+      return false;
+    }
+  }
+
+export { storeDate, storeArray, isDateCached, isCached }
--- src/pages/api/sales/kiwify.js
+++ src/pages/api/sales/kiwify.js
@@ -1,50 +1,7 @@
 import clientPromise from '@/lib/mongo/mongo';
 import keyv from '@/lib/cache/keyv';
+import { storeDate, storeArray, isDateCached, isCached } from '@/lib/cache/cache';
 import { encodeBase64 } from '@/lib/string/base64';
-
-async function storeDate(initial, end) {
-  try {
-    const clientCache = await keyv;
-    await clientCache.set('initialDate', initial);
-    await clientCache.set('endDate', end);
-  } catch (error) {
-    console.error(`Error storing dates: ${error}`);
-  }
-}
-
-async function storeArray(array) {
-  try {
-    const clientCache = await keyv;
-    await clientCache.set('sales', array);
-  } catch (error) {
-    console.error(`Error storing array: ${error}`);
-  }
-}
-
-async function isDateCached(initial, end) {
-  try {
-    const clientCache = await keyv;
-    const [initialDate, endDate] = await Promise.all([
-      clientCache.get('initialDate'),
-      clientCache.get('endDate')
-    ]);
-    return initialDate === initial && endDate === end;
-  } catch (error) {
-    console.error(`Error checking date cache: ${error}`);
-    return false;
-  }
-}
-
-async function isCached() {
-  try {
-    const clientCache = await keyv;
-    const sales = await clientCache.get('sales');
-    return Boolean(sales);
-  } catch (error) {
-    console.error(`Error checking sales cache: ${error}`);
-    return false;
-  }
-}
 
 export default async function getSales(req, res) {
   try {
